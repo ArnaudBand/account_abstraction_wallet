6e26b34e0ec6a1fae807df91540b5442e4d71c16
Arnaud Bandonkeye <bandonkeyea@gmail.com>
2024-07-11 20:44:51 +0000

    feat: Add DeployMinimalAccount contract

---
+++ script/DeployMinimalAccount.s.sol
@@ -0,0 +1,7 @@
+// SPDX-License-Identifier: SEE LICENSE IN LICENSE
+pragma solidity ^0.8.24;
+
+import {Script} from "forge-std/Script.sol";
+import {MinimalAccount} from "../src/ethereum/MinimalAccount.sol";
+
+contract DeployMinimalAccount {}
