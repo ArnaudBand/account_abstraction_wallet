497d34986ebdda862b9eb53a19902452fb7ccab5
Arnaud Bandonkeye <bandonkeyea@gmail.com>
2024-07-11 21:22:54 +0000

    feat: Add deployment script for MinimalAccount contract

--- script/DeployMinimalAccount.s.sol
+++ script/DeployMinimalAccount.s.sol
@@ -3,5 +3,20 @@ pragma solidity ^0.8.24;
 
 import {Script} from "forge-std/Script.sol";
 import {MinimalAccount} from "../src/ethereum/MinimalAccount.sol";
+import {HelperConfig} from "../script/HelperConfig.s.sol";
 
-contract DeployMinimalAccount {}
+contract DeployMinimalAccount is Script {
+
+  function run() public returns (HelperConfig, MinimalAccount) {
+    return deployMinimalAccount();
+  }
+    function deployMinimalAccount() public returns (HelperConfig, MinimalAccount) {
+        HelperConfig helperConfig = new HelperConfig();
+        HelperConfig.NetworkConfig memory networkConfig = helperConfig.getConfig();
+        vm.startBroadcast(networkConfig.account);
+        MinimalAccount minimalAccount = new MinimalAccount(networkConfig.entryPoint);
+        minimalAccount.transferOwnership(msg.sender);
+        vm.stopBroadcast();
+        return (helperConfig, minimalAccount);
+    }
+}
