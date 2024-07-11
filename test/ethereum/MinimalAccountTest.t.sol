e1573568a3e21bc4cd94052a105b00400fce7e28
Arnaud Bandonkeye <bandonkeyea@gmail.com>
2024-07-11 21:57:44 +0000

    refactor: Update execute function in MinimalAccountTest contract to use requireFromEntryPointOrOwner modifier

---
+++ test/ethereum/MinimalAccountTest.t.sol
@@ -0,0 +1,35 @@
+// SPDX-License-Identifier: SEE LICENSE IN LICENSE
+pragma solidity ^0.8.24;
+
+import {Test} from "forge-std/Test.sol";
+import {MinimalAccount} from "../../src/ethereum/MinimalAccount.sol";
+import {DeployMinimalAccount} from "../../script/DeployMinimalAccount.s.sol";
+import {HelperConfig} from "../../script/HelperConfig.s.sol";
+import {ERC20Mock} from "@openzeppelin/contracts/mocks/token/ERC20Mock.sol";
+
+contract MinimalAccountTest is Test {
+    MinimalAccount public minimalAccount;
+    HelperConfig public config;
+    DeployMinimalAccount public deployer;
+    ERC20Mock public usdc;
+    uint256 constant AMOUNT = 1e18;
+
+    function setUp() public {
+        deployer = new DeployMinimalAccount();
+        (config, minimalAccount) = deployer.deployMinimalAccount();
+        usdc = new ERC20Mock();
+    }
+
+    function testOwnerCanExecuteComands() public {
+        assertEq(usdc.balanceOf(address(minimalAccount)), 0);
+        address dest = address(usdc);
+        uint256 value = 0;
+        bytes memory funcData = abi.encodeWithSelector(ERC20Mock.mint.selector, address(minimalAccount), AMOUNT);
+
+        vm.prank(minimalAccount.owner());
+        minimalAccount.execute(dest, value, funcData);
+
+        assertEq(usdc.balanceOf(address(minimalAccount)), AMOUNT);
+    }
+}
+
