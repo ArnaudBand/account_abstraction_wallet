e3f9a6a839ec34ba83ce50a5fa988cb972c710a8
Arnaud Bandonkeye <bandonkeyea@gmail.com>
2024-07-11 21:20:56 +0000

    feat: Update HelperConfig contract with BURNER_WALLET constant

--- script/HelperConfig.s.sol
+++ script/HelperConfig.s.sol
@@ -10,27 +10,29 @@ contract HelperConfig is Script {
 
     struct NetworkConfig {
         address entryPoint;
+        address account;
     }
 
     uint256 constant ETH_SEPOLIA_CHAIN_ID = 11155111;
     uint256 constant ZKSYNC_SEPOLIA_CHAIN_ID = 300;
     uint256 constant LOCAL_CHAIN_ID = 31337;
+    address constant BURNER_WALLET = 0x9C8A2750E8814eDF52224a92bD61B4F596a94c17;
 
     NetworkConfig public localNetworkConfig;
-    mapping (uint256 chainId => NetworkConfig) public networkConfigs;
+    mapping(uint256 chainId => NetworkConfig) public networkConfigs;
 
     constructor() {
-      networkConfigs[ETH_SEPOLIA_CHAIN_ID] = getSepoliaConfig();
+        networkConfigs[ETH_SEPOLIA_CHAIN_ID] = getSepoliaConfig();
     }
 
-    function getConfig() public view returns(NetworkConfig memory) {
+    function getConfig() public view returns (NetworkConfig memory) {
         return getConfigByChainId(block.chainid);
     }
 
     function getConfigByChainId(uint256 chainId) public view returns (NetworkConfig memory) {
         if (chainId == LOCAL_CHAIN_ID) {
             return getOrCreateAnvilEthConfig();
-        } else if (networkConfigs[chainId].entryPoint != address(0)) {
+        } else if (networkConfigs[chainId].account != address(0)) {
             return networkConfigs[chainId];
         } else {
             revert HelperConfig__InvalidChain();
@@ -38,19 +40,15 @@ contract HelperConfig is Script {
     }
 
     function getSepoliaConfig() public pure returns (NetworkConfig memory) {
-        return NetworkConfig({
-            entryPoint: 0x5FF137D4b0FDCD49DcA30c7CF57E578a026d2789
-        });
+        return NetworkConfig({entryPoint: 0x5FF137D4b0FDCD49DcA30c7CF57E578a026d2789, account: BURNER_WALLET});
     }
 
     function getZkSyncSepoliaConfig() public pure returns (NetworkConfig memory) {
-        return NetworkConfig({
-            entryPoint: address(0)
-        });
+        return NetworkConfig({entryPoint: address(0), account: BURNER_WALLET});
     }
 
     function getOrCreateAnvilEthConfig() public view returns (NetworkConfig memory) {
-        if (localNetworkConfig.entryPoint != address(0)) {
+        if (localNetworkConfig.account != address(0)) {
             return localNetworkConfig;
         }
 
