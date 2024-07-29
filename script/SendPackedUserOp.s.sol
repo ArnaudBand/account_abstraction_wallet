5c5154fdc087473c443de8d2ffab9b11d5b825f7
Arnaud Bandonkeye <bandonkeyea@gmail.com>
2024-07-29 14:54:10 +0000

    Import struct PackedUserOperation

--- script/SendPackedUserOp.s.sol
+++ script/SendPackedUserOp.s.sol
@@ -1,11 +1,15 @@
-// SPDX-License-Identifier: SEE LICENSE IN LICENSE
+// SPDX-License-Identifier: MIT
 pragma solidity ^0.8.24;
 
 import {Script} from "forge-std/Script.sol";
+import {PackedUserOperation} from "@account-abstraction/interfaces/PackedUserOperation.sol";
 import {MinimalAccount} from "../src/ethereum/MinimalAccount.sol";
 
 contract SendPackedUserOp is Script {
     function run() public {}
 
-    function deployMinimalAccount() public {}
+    function deployMinimalAccount() public return (PackedUserOperation memory) {
+        // 1. Generate the unsigned date
+        // 2. Sign it and return it.
+    }
 }
