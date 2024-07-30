79f7bea4fecdac1c34010227f07f54766d3e7087
Arnaud Bandonkeye <bandonkeyea@gmail.com>
2024-07-30 10:21:46 +0000

    refactor: Update SendPackedUserOp contract to generate signed user operation

--- script/SendPackedUserOp.s.sol
+++ script/SendPackedUserOp.s.sol
@@ -3,17 +3,47 @@ pragma solidity ^0.8.24;
 
 import {Script} from "forge-std/Script.sol";
 import {PackedUserOperation} from "@account-abstraction/interfaces/PackedUserOperation.sol";
-import {MinimalAccount} from "../src/ethereum/MinimalAccount.sol";
+import {HelperConfig} from "../script/HelperConfig.s.sol";
+import {IEntryPoint} from "@account-abstraction/interfaces/IEntryPoint.sol";
+import {MessageHashUtils} from "@openzeppelin/contracts/utils/cryptography/MessageHashUtils.sol";
 
 contract SendPackedUserOp is Script {
+    using MessageHashUtils for bytes32;
+
     function run() public {}
 
-    function deployMinimalAccount() public returns(PackedUserOperation memory) {
-        // 1. Generate the unsigned date
+    function generateSignedUserOperation(bytes memory callData, HelperConfig.NetworkConfig memory config)
+        public
+        view
+        returns (PackedUserOperation memory)
+    {
+        // 1. Generate the unsigned data
+        uint256 nonce = vm.getNonce(config.account);
+        PackedUserOperation memory userOp = _generateUnsignedUserOperation(callData, config.account, nonce);
+
+        // 2. Get the UserOp Hash
+        bytes32 userOpHash = IEntryPoint(config.entryPoint).getUserOpHash(userOp);
+        bytes32 digest = userOpHash.toEthSignedMessageHash();
         // 2. Sign it and return it.
+        uint8 v;
+        bytes32 r;
+        bytes32 s;
+        uint256 chainId = block.chainid;
+        uint256 ANVIL_DEFAULT_KEY = 0xac0974bec39a17e36ba4a6b4d238ff944bacb478cbed5efcae784d7bf4f2ff80;
+        if (chainId == 31337) {
+            (v, r, s) = vm.sign(ANVIL_DEFAULT_KEY, digest);
+        } else {
+            (v, r, s) = vm.sign(config.account, digest);
+        }
+        userOp.signature = abi.encodePacked(r, s, v);
+        return userOp;
     }
 
-    function _generateUnsignedUserOperation(bytes memory calldata, address sender, uint256 nonce) internal pure returns(PackedUserOperation memory) {
+    function _generateUnsignedUserOperation(bytes memory callData, address sender, uint256 nonce)
+        internal
+        pure
+        returns (PackedUserOperation memory)
+    {
         uint128 verificationGasLimit = 16777216;
         uint128 callGasLimit = verificationGasLimit;
         uint128 maxPriorityFeePerGas = 256;
@@ -22,12 +52,12 @@ contract SendPackedUserOp is Script {
             sender: sender,
             nonce: nonce,
             initCode: hex"",
-            callData: calldata,
+            callData: callData,
             accountGasLimits: bytes32(uint256(verificationGasLimit) << 128 | callGasLimit),
             preVerificationGas: verificationGasLimit,
-            gasFee: bytes32(uint256(maxPriorityFeePerGas) << 128 | maxFeePerGas),
+            gasFees: bytes32(uint256(maxPriorityFeePerGas) << 128 | maxFeePerGas),
             paymasterAndData: hex"",
             signature: hex""
-        })
+        });
     }
 }
